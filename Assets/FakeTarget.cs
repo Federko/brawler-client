4b471c2b29e69b154128d22787be2be6e73000b2
MattiaPezzanoAiv <mattiapezzano7@gmail.com>
2017-03-29 19:13:04 +0200

    Added camera souls like, action and lock state, with target switch

---
+++ Assets/FakeTarget.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeTarget : MonoBehaviour,ITargettable {
+    public Transform Transform
+    {
+        get { return transform; }
+    }
+
+    //public event EventHandler OnTargetFocused;
+    //public event EventHandler OnTargetLosed;
+    //public event EventHandler OnTargetReleased;
+
+    public void OnTargetFocused()
+    {
+        Debug.Log("TARGET FOCUSED");
+    }
+
+    public void OnTargetLosed()
+    {
+        Debug.Log("TARGET LOSED");
+    }
+
+    public void OnTargetReleased()
+    {
+        Debug.Log("TARGET RELEASED");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    public Transform target;
+	// Update is called once per frame
+	void Update () {
+
+        transform.RotateAround(target.position,Vector3.up, 100 * Time.deltaTime);
+	}
+}
